668db73833ed1b9411a60b05669d8cb2b2b63770
Brian West <brianwilliamwest@gmail.com>
2016-10-26 15:12:16 -0400

    Added table view controller

---
+++ BudgetApp/BudgetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BudgetTableViewCell.swift
+//  BudgetApp
+//
+//  Created by Brian West on 10/26/16.
+//  Copyright © 2016 FishWare. All rights reserved.
+//
+
+import UIKit
+
+class BudgetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
